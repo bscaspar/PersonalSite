62f8e7047f00e6dbfa89f1d62a558744b26409b4
Brady Caspar <Brady@Bradys-MacBook-Air.local>
2018-10-19 14:42:31 -0700

    about me link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import WelcomePage from './WelcomePage';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const welcomeMessage = { 
         welcome: "Hi",
         name: "I'm Brady",
         site: "This is my new site",
-        description: "I am a new web developer and will be using this site as a personal design project, a testing ground for new ideas and frameworks (if you find anything broken, email me!) and a showcase of what I can do. For more info about me, click here."
+        description: (<div>I am a new web developer and will be using this site as a personal design project, a testing ground for new ideas and frameworks (if you find anything broken, email me!) and a showcase of what I can do. For more info about me,
+                <Link to="/aboutme">
+                    &nbsp;click here
+                </Link>
+                .
+            </div>)
     }
 
     return (
