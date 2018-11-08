d2e62319a4470c57def4b4c89187e2e92ac5c3c3
bscaspar <bradyscaspar@outlook.com>
2018-11-08 15:06:07 -0800

    favicon

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,52 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Grid, Typography } from "@material-ui/core";
 
-import SimpleAppBar from './simple_appbar';
-import HeaderPaper from './header_paper';
-import BodyPaper from './body_paper';
+import SimpleAppBar from "./simple_appbar";
+import HeaderPaper from "./header_paper";
+import BodyPaper from "./body_paper";
+import icon from "../../assets/icons/favicon-32x32.png";
 
 const styles = {
-    root: {
-        flexGrow: 1
-    }
+  root: {
+    flexGrow: 1
+  },
+  footer: {
+    float: "right",
+    display: "table"
+  },
+  footerText: {
+    display: "table-cell",
+    verticalAlign: "middle"
+  }
 };
 
-const HomeGrid = (props) => {
-    const { classes } = props;
+const HomeGrid = props => {
+  const { classes } = props;
 
-    return (
-        <React.Fragment>
-            <SimpleAppBar />
-            <main>
-                <Grid container className={classes.root}>
-                    <Grid item sm={1} />
-                    <Grid item sm={10}>
-                        <HeaderPaper />
-                        <BodyPaper />
-                    </Grid>
-                    <Grid item sm={1} />
-                </Grid>
-            </main>    
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <SimpleAppBar />
+      <main>
+        <Grid container className={classes.root}>
+          <Grid item sm={1} />
+          <Grid item sm={10}>
+            <HeaderPaper />
+            <BodyPaper />
+          </Grid>
+          <Grid item sm={1} />
+        </Grid>
+      </main>
+      <footer>
+        <div className={classes.footer}>
+          <Typography className={classes.footerText} variant="caption">
+            © Brady Caspar {new Date().getFullYear()}
+          </Typography>
+          <img src={icon} />
+        </div>
+      </footer>
+    </React.Fragment>
+  );
 };
 
 export default withStyles(styles)(HomeGrid);
--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
-import customTheme from './theme/theme';
-import '../styles/style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
+import customTheme from "./theme/theme";
+import "../styles/style.css";
+import "../assets/icons/favicon.ico";
 
-import Home from './components/home'; 
+import Home from "./components/home";
 
 const App = () => {
-    return(
-        <React.Fragment>
-        <CssBaseline />
-        <MuiThemeProvider theme={customTheme}>
-            <Home />
-        </MuiThemeProvider>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <MuiThemeProvider theme={customTheme}>
+        <Home />
+      </MuiThemeProvider>
+    </React.Fragment>
+  );
+};
 
-ReactDOM.render(
-    <App />, 
-    document.querySelector('.app'));
+ReactDOM.render(<App />, document.querySelector(".app"));
